d43ff0e66aabbc12702f4eb2443445dbbcf4e644
Eric11195 <izandeve@ucm.es>
2026-04-07 23:37:41 +0200

    Simple scene

---
+++ New Unity Project/Assets/Scripts/CardComponent.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CardComponent : MonoBehaviour
+{
+    [SerializeField]
+    int card_id;
+    void set_card(int card_id)
+    {
+        //Set card image
+    }
+}
