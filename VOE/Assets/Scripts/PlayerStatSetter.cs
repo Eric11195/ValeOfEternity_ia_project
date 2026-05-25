b02e0f9db9fdb576ff82693bb6f5dd993adbc973
Eric11195 <149362942+IzanDeVegaLopez@users.noreply.github.com>
2026-05-25 11:23:49 +0200

    show hide metricas

--- VOE/Assets/Scripts/PlayerStatSetter.cs
+++ VOE/Assets/Scripts/PlayerStatSetter.cs
@@ -8,13 +8,30 @@ namespace voe
     public class PlayerStatSetter : MonoBehaviour
     {
         List<PlayerStatSetterInPannel> psspl;
+        private GameObject activator_button;
+        private bool is_visible = false;
         private void Start()
         {
             psspl = new List<PlayerStatSetterInPannel>();
+            int i = 0;
             foreach(Transform tr in this.transform)
             {
                 psspl.Add(tr.GetComponent<PlayerStatSetterInPannel>());
                 tr.gameObject.SetActive(false);
+                ++i;
+                if (i == 4) break;
+            }
+            activator_button = GameObject.Find("Activator");
+            activator_button.SetActive(false);
+            is_visible = false;
+        }
+
+        public void show_hide()
+        {
+            is_visible = !is_visible;
+            foreach (var p in psspl)
+            {
+                p.gameObject.SetActive(is_visible);
             }
         }
         public void set_stats(stats[] st)
@@ -26,6 +43,8 @@ namespace voe
                 psspl[i].gameObject.SetActive(true);
                 psspl[i].set_stats(st[i]);
             }
+            activator_button.SetActive(true);
+            is_visible = true ;
         }
     }
 }
